54523481e43d53033fd484154e6068e9ef912aa7
robertstevens <robert@stevens.biz>
2019-06-17 21:53:51 +0200

    Working on the file loader

--- src/test/java/biz/stevens/config/server/SpringBootConfigApplicationLocalFilesTest.java
+++ src/test/java/biz/stevens/config/server/SpringBootConfigApplicationLocalFilesTest.java
@@ -1,23 +1,20 @@
 package biz.stevens.config.server;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.cloud.config.environment.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
 
 @ActiveProfiles("native")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootConfigApplicationLocalFilesTest {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -27,9 +24,12 @@ public class SpringBootConfigApplicationLocalFilesTest {
     }
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class);
+    public void shouldReturnExpectedConfig() throws Exception {
+        Environment out = this.restTemplate.getForObject("/app/profile/",
+                Environment.class);
+        Assert.assertEquals("app", out.getName());
+        Assert.assertEquals("profile", out.getProfiles()[0]);
+        Assert.assertFalse(out.getPropertySources().isEmpty());
+        Assert.assertEquals("world", out.getPropertySources().get(0).getSource().get("hello"));
     }
-
 }
