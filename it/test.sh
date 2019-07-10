d080c0c1072aef2d1197188001ce6dc5bd41dbf4
Robert Stevens <robert@stevens.tk>
2019-07-10 18:40:18 +0200

    Updated the test

--- it/test.sh
+++ it/test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sleep 10
-curl -sS http://config-server:8080/health || exit 1
+curl --connect-timeout 5 \
+    --max-time 10 \
+    --retry 10 \
+    -sS http://config-server:8080/health || exit 1
